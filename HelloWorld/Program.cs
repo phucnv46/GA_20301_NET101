34b967f70aa953f412c55632232d23df5f3b45b7
Phuc Nguyen Van <phucnv46@fe.edu.vn>
2025-01-07 15:32:41 +0700

    Update nhap

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,21 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Xin moi nhap: ");
+            string input = Console.ReadLine(); // nhap vao 1 dong
+
             Console.WriteLine("Hello, World!");
             //Viet nhanh Console.WriteLine() => cw + tab
-            Console.WriteLine("Toi ten la Phuc");
+            Console.WriteLine($"Ban vua nhap la: {input} " );
+
+            //Chan ly 
+            bool DungSai = true; // true / false
+            // object => bo cua cac kieu khac 
+            // khai bao ngam dinh
+
+            
+
+
         }
     }
 }
