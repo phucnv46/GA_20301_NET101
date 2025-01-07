f74c94eac17f237a77241e560b7d0d911c36206c
Phuc Nguyen Van <phucnv46@fe.edu.vn>
2025-01-07 15:13:07 +0700

    Hello git

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Viet nhanh Console.WriteLine() => cw + tab
+            Console.WriteLine("Toi ten la Phuc");
+        }
+    }
+}
